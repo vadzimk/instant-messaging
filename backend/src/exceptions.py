244cc0a020c8f6ea88d65bf97413c4fae4ba002b
Vadzim <vadzimk@hotmail.com>
2024-11-08 22:51:24 +0300

    backend: fix: global custom exceptions handler not catching custom exceptions

--- backend/src/exceptions.py
+++ backend/src/exceptions.py
@@ -1,14 +1,5 @@
-from fastapi import HTTPException
-from starlette import status
-
-
-class CouldNotValidateCredentials(HTTPException):
-    def __init__(self, message="Could not validate credentials"):
-        super().__init__(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail=message,
-            headers={"WWW-Authenticate": "Bearer"},
-        )
+class CouldNotValidateCredentials(Exception):
+    pass
 
 
 class EntityNotFoundError(Exception):
