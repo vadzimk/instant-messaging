8c772dc7ac2c494cb75a1437c31c27e48542b3f6
Vadzim <vadzimk@hotmail.com>
2024-11-10 00:04:30 +0300

    bot: subscribe to notifications and test

--- telegram_bot/src/routers.py
+++ telegram_bot/src/routers.py
@@ -14,7 +14,6 @@ async def start(message: types.Message):
 async def capture_email(message: types.Message):
     tg_user_id = message.from_user.id
     email = message.text
-    if await NotificationService.subscribe_user(tg_user_id, email):
-        await message.answer("You've subscribed to message notifications")
-    else:
-        await message.answer('Use /start to register for notifications')
+    await message.answer(
+        await NotificationService.subscribe_user(tg_user_id, email)
+    )
--- telegram_bot/src/services/notification_service.py
+++ telegram_bot/src/services/notification_service.py
@@ -1,15 +1,24 @@
+import logging
+
 import aiohttp
 
-API_BASE_URL = 'http://localhost:8000'
+logger = logging.getLogger(__name__)
+
+API_BASE_URL = 'http://localhost:8000/service/telegram'
 
 
 class NotificationService:
     @classmethod
-    async def subscribe_user(cls, tg_user_id: int, user_email: str) -> bool:
-        async with aiohttp.ClientSession() as session:
-            async with session.post(API_BASE_URL, json={}) as res:
-                data = await res.json()
-                if res.status == 200:
-                    return True
-                else:
-                    return False
+    async def subscribe_user(cls, tg_user_id: int, user_email: str) -> str:
+        user_data = {'tg_user_id': tg_user_id, 'user_email': user_email}
+        try:
+            async with aiohttp.ClientSession() as session:
+                async with session.post(API_BASE_URL + '/subscriptions', json=user_data) as res:
+                    if res.status == 204:
+                        return "You've subscribed to message notifications"
+                    else:
+                        logger.error(f'Failed to subscribe. Status code: {res.status}. Response: {await res.json()}')
+                        return f"Failed to subscribe\n{(await res.json()).get('detail')}\nUse /start to try again to register for notifications"
+        except aiohttp.ClientError as e:
+            logger.error(f'Error wile subscribing user: {str(e)}')
+            return "Failed to subscribe\nInternal server error"
