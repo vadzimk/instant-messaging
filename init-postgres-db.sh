8eb1fa69567c9fe4b506526abfc75eccf2b7c007
Vadzim <vadzimk@hotmail.com>
2024-11-26 09:30:28 +0300

    Update .gitlab-ci.yml file
    check connection to postgress

--- init-postgres-db.sh
+++ init-postgres-db.sh
@@ -27,39 +27,39 @@
 set -eu; # if varibales unset raise error, if error terminate
 export PGPASSWORD="$POSTGRES_PASSWORD";
 
-# Create database if not exists
-echo "Creating database: ${DATABASE_NAME}"
-if psql -h "$DATABASE_HOST" -p "$DATABASE_PORT" -U "$POSTGRES_USER" -lqt | cut -d \| -f 1 | grep -qw "$DATABASE_NAME";
-then
-    echo "${DATABASE_NAME} already exists. Skipping...";
-else
-    if createdb -h "$DATABASE_HOST" -p "$DATABASE_PORT" -U "$POSTGRES_USER" -T template0 "$DATABASE_NAME"; then
-        echo "${DATABASE_NAME} created";
-        if ! ./restore.sh "$DATABASE_NAME"; then
-            echo "Error restoring database" >&2;
-            exit 1;
-        fi;
-    else
-        echo "Error creating database" >&2;
-        exit 1;
-    fi;
-fi;
-
-# Create user if it doesn't exist
-echo "Creating user: ${TARGET_USER}"
-USER_EXISTS=$(psql -h "$DATABASE_HOST" -p "$DATABASE_PORT" -U "$POSTGRES_USER" -tc \
-"SELECT 1 FROM pg_roles WHERE rolname = '$TARGET_USER';" | xargs)
-if [ "$USER_EXISTS" != '1' ]; then
-    if psql -h "$DATABASE_HOST" -p "$DATABASE_PORT" -U "$POSTGRES_USER" -c \
-    "CREATE USER \"$TARGET_USER\" WITH PASSWORD '$TARGET_USER_PASSWORD';"; then
-        echo "User ${TARGET_USER} created";
-    else
-        echo "Error creating user $TARGET_USER" >&2;
-        exit 1;
-    fi;
-else
-    echo "User ${TARGET_USER} already exists. Skipping..."
-fi;
+## Create database if not exists
+#echo "Creating database: ${DATABASE_NAME}"
+#if psql -h "$DATABASE_HOST" -p "$DATABASE_PORT" -U "$POSTGRES_USER" -lqt | cut -d \| -f 1 | grep -qw "$DATABASE_NAME";
+#then
+#    echo "${DATABASE_NAME} already exists. Skipping...";
+#else
+#    if createdb -h "$DATABASE_HOST" -p "$DATABASE_PORT" -U "$POSTGRES_USER" -T template0 "$DATABASE_NAME"; then
+#        echo "${DATABASE_NAME} created";
+#        if ! ./restore.sh "$DATABASE_NAME"; then
+#            echo "Error restoring database" >&2;
+#            exit 1;
+#        fi;
+#    else
+#        echo "Error creating database" >&2;
+#        exit 1;
+#    fi;
+#fi;
+#
+## Create user if it doesn't exist
+#echo "Creating user: ${TARGET_USER}"
+#USER_EXISTS=$(psql -h "$DATABASE_HOST" -p "$DATABASE_PORT" -U "$POSTGRES_USER" -tc \
+#"SELECT 1 FROM pg_roles WHERE rolname = '$TARGET_USER';" | xargs)
+#if [ "$USER_EXISTS" != '1' ]; then
+#    if psql -h "$DATABASE_HOST" -p "$DATABASE_PORT" -U "$POSTGRES_USER" -c \
+#    "CREATE USER \"$TARGET_USER\" WITH PASSWORD '$TARGET_USER_PASSWORD';"; then
+#        echo "User ${TARGET_USER} created";
+#    else
+#        echo "Error creating user $TARGET_USER" >&2;
+#        exit 1;
+#    fi;
+#else
+#    echo "User ${TARGET_USER} already exists. Skipping..."
+#fi;
 
 # Grant user privileges to the database
 echo "Granting privileges to user: ${TARGET_USER} for database: ${DATABASE_NAME} "
